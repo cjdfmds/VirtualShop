3c8d7d11669d74902c6c09c73733c273a74fd7e9
Zerian <48261394+cjdfmds@users.noreply.github.com>
2024-11-12 09:50:49 +1300

    Initial commit for virtual shop project with Vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import Scene from './components/scene';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Virtual Shop</h1>
+      <Scene />
+    </div>
+  );
+}
+
+export default App;
